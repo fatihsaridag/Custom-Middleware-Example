98594c94e96fc5c4d1aca8b723f3c20b4844cb5e
Fatih Saridag <fatihsaridag26@gmail.com>
2022-11-28 11:45:12 +0300

    Custom Middleware Oluşturma

---
+++ Custom Middleware Example/Extensions/Extension.cs
@@ -0,0 +1,13 @@
+﻿using Custom_Middleware_Example.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace Custom_Middleware_Example.Extensions
+{
+    static public class Extension
+    {
+        public static IApplicationBuilder UseFatih(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<FatihMiddleware>();
+        }
+    }
+}
---
+++ Custom Middleware Example/Middlewares/FatihMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace Custom_Middleware_Example.Middlewares
+{
+    public class FatihMiddleware
+    {
+        RequestDelegate _next;
+
+        public FatihMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            //Custom Operasyon
+            Console.WriteLine("Fatih Sarıdağ Başlangıç Middleware");
+            await _next.Invoke(httpContext); // Bizden sonraki middlewarei tetikledik.
+            Console.WriteLine("Fatih Sarıdağ Bitiş Middleware"); // Middlewareden tekrar geldikten sonra response olarak 
+
+
+        }
+
+
+    }
+}
